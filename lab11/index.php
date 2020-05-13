08614ea588b584ae4e31433f50d3dc4f7332a621
daniil <daniil@clean.email>
2020-05-13 23:35:12 +0300

    remove politeh

--- lab11/index.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-require_once 'application/core/model.php';
-require_once 'application/core/view.php';
-require_once 'application/core/controller.php';
-require_once 'application/core/route.php';
-
-Route::start();
-
-ini_set('display_errors', 1);
--- lab14/application/core/config.php
+++
@@ -1,4 +0,0 @@
-<?
-  $dbconn = pg_connect("host=localhost dbname=postgres user=postgres password=Mostwanted1")
-  or die('Не удалось соединиться: ' . pg_last_error());
-?>
